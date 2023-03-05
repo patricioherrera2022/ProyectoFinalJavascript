063c89f0c3208436e9fee725514bad62efcc41bb
patricioherrera2022 <herrerapatricio281@gmail.com>
2023-03-04 21:21:22 -0300

    3er Entrega

---
+++ data/stock.js
@@ -0,0 +1,44 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Comunity Managger",
+        tipo: "digital",
+        desc: "Tene la tranquilidad de dejar en buenas manos tus redes sociales. Nosotros nos encargamos de todo!",
+        precio: "$ " + 6500,
+        imagen: './img/cm.png',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Diseño gráfico",
+        tipo: "Digital",
+        desc: "Realizamos piezas gráficas de alta calidad!",
+        precio: "$ " + 8000,
+
+        imagen: './img/diseñlo.png',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Desarrollo web",
+        tipo: "Digital",
+        desc: "Realizamos maquetación, planificación y desarrollo de páginas web",
+        precio: "$ " +18000,
+        imagen: './img/web.png',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Filtros Instagram & Facebook",
+        tipo: "Digital",
+        desc: "Realizamos filtros para Instagram & Facebook, ideal para tu comunidad",
+        precio: "$ " +1600,
+        imagen: './img/filtro.png',
+        cantidad:1
+    },
+
+];
---
+++ js/home.js
@@ -0,0 +1,18 @@
+const pintarProductos = (data) => {
+    const contenedor = document.getElementById("producto-contenedor");
+
+    data.forEach(producto => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML += `<div class="card-image">
+                          <img class="img2" src=${producto.imagen}>
+                          <span class="card-title">${producto.nombre}</span> <br> <br>
+                          <button class="botoncito" <a class="btn-floating halfway-fab wabes-effect waves-light red"><i id=${producto.id}  class="material-icons agregar">¡Agregar!</i></a></button>
+                        </div> 
+                        <div class="card-content">
+                            <p>${producto.desc}</p>
+                            <p>${producto.precio}</p>
+                        </div>`
+      contenedor.appendChild(div);
+    });
+  };
