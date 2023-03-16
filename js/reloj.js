a06cf2349e60b8a5a427539c47fed1ed3cc5be9d
patricioherrera2022 <herrerapatricio281@gmail.com>
2023-03-16 17:06:50 -0300

    actualización del proyecto!

---
+++ js/reloj.js
@@ -0,0 +1,21 @@
+const $tiempo = document.querySelector('.tiempo'),
+$fecha = document.querySelector('.fecha');
+
+function digitalClock(){
+    let f = new Date(),
+    dia = f.getDate(),
+    mes = f.getMonth() + 1,
+    anio = f.getFullYear(),
+    diaSemana = f.getDay();
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2)
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
